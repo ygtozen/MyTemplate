cdb620256ab91f25e3ff472c6f355967c3c5a8e0
ygtozen <ozenyigit151@gmail.com>
2022-06-13 15:36:30 +0300

    card revizee

---
+++ template.js
@@ -0,0 +1,46 @@
+$(function () {
+  // auto-complete
+  var availableTags = [
+    'ActionScript',
+    'AppleScript',
+    'Asp',
+    'BASIC',
+    'C',
+    'C++',
+    'Clojure',
+    'COBOL',
+    'ColdFusion',
+    'Erlang',
+    'Fortran',
+    'Groovy',
+    'Haskell',
+    'Java',
+    'JavaScript',
+    'Lisp',
+    'Perl',
+    'PHP',
+    'Python',
+    'Ruby',
+    'Scala',
+    'Scheme',
+  ]
+  $('#search').autocomplete({
+    source: availableTags,
+  })
+
+
+
+
+  //date
+  let showDate=()=>{
+      return new Date();
+  }
+
+//toggle show/hide
+$("#date_hide").html(showDate())
+$("#show_hide_toggler").click(function(){
+    $("#date_hide").toggle(1000)
+})
+
+
+})
